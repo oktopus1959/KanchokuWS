8100a455094ddfd776fb784adc0e13419ef80711
OKA Toshiyuki <oktopus1959@gmail.com>
2021-07-11 15:07:17 +0900

    目次作成スクリプトの追加

---
+++ make_toc.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+def make_toc_line(s)
+    return "[#{s}](\##{s.gsub(/ +/, '-').gsub(/[.,()]/,'')})"
+end
+
+while line = gets
+  line = line.strip
+  if line =~ /^## +(.*[^\s])\s*$/ && line != "## 目次"
+    puts "- #{make_toc_line($1)}"
+  elsif line =~ /^### +(.*[^\s])\s*$/
+    puts "    - #{make_toc_line($1)}"
+  elsif line =~ /^#### +(.*[^\s])\s*$/
+    puts "        - #{make_toc_line($1)}"
+  end
+end
+
