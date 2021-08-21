8fe0a4bfe13515950003e363890020a5df152023
OKA Toshiyuki <oktopus1959@gmail.com>
2021-08-21 23:16:10 +0900

    やはり読みと変換形が同一のものは受け入れるようにした
      - 「国民は」の「国民」は通す必要あり。そうしないと「国民派」に変換されてしまう

--- kw-uni/Mazegaki/MazegakiDic.cpp
+++ kw-uni/Mazegaki/MazegakiDic.cpp
@@ -769,7 +769,9 @@ namespace {
                             _LOG_DEBUGH(_T("key=%s, keyStem=%s, mazeSearch=%s, p->stem=%s, p->xfer=%s, user=%s, deleted=%s"),
                                 MAKE_WPTR(key), MAKE_WPTR(keyStem), BOOL_TO_WPTR(mazeSearch), MAKE_WPTR(p->stem), MAKE_WPTR(p->xfer), BOOL_TO_WPTR(p->userDic), BOOL_TO_WPTR(p->deleted));
                             if (p->deleted) continue;
-                            if (/*keyStem != p->xfer &&*/ utils::startsWith(keyStem, p->xfer)) continue;   // 読み語幹の先頭部が変換形と一致したものは除外(「代表しゃ」が「代表/する」の語幹+「し」にマッチするケースや「経い」→「経」のケース)
+                            // 読み語幹の先頭部が変換形と一致したものは除外(「代表しゃ」が「代表/する」の語幹+「し」にマッチするケースや「経い」→「経」のケース)
+                            // ただし、「国民は」の「国民」は通す必要あり。そうしないと「国民派」に変換されてしまう
+                            if (keyStem != p->xfer && utils::startsWith(keyStem, p->xfer)) continue;
 
                             if (keyStem == p->stem || mazeSearch && order_matched(keyStem, p)) {
                                 // 読み語幹が完全一致、または key に漢字が含まれている場合は、ひらがな・漢字の出現順序の一致を確認
