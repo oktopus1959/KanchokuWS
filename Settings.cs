6ea04c687d6bf6272a2fa4d06e956cd30a30ab57
OKA Toshiyuki <oktopus1959@gmail.com>
2021-08-28 11:26:52 +0900

    version

--- Settings.cs
+++ Settings.cs
@@ -11,7 +11,7 @@ namespace KanchokuWS
     {
         //-------------------------------------------------------------------------------------
         /// <summary> バージョン </summary>
-        public static string Version => "1.1.1";
+        public static string Version => "1.1.2";
 
         /// <summary> ドキュメントへのURL </summary>
         public static string DocumentUrl => "https://github.com/oktopus1959/KanchokuWS#readme";
