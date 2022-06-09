7e68d681e304600fa223db318cea9843aefb7578
OKA Toshiyuki <oktopus1959@gmail.com>
2022-06-09 22:56:24 +0900

    add UtilsTest

---
+++ UtilsTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Utils;
+
+namespace UtilsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger.LogFilename = "UtilsTest.log";
+            Logger.LogLevel = Logger.LogLevelInfo;
+
+            new UtilsTest().loggerTest();
+        }
+    }
+}
---
+++ UtilsTest/UtilsTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Utils;
+
+namespace UtilsTest
+{
+    class UtilsTest
+    {
+        private static Logger logger = Logger.GetLogger(true);
+
+        public void loggerTest()
+        {
+            logger.Debug(() => $"CALLED: {sub()}");
+        }
+
+        private string sub()
+        {
+            logger.Info("CALLED");
+            return "foo";
+        }
+    }
+}
