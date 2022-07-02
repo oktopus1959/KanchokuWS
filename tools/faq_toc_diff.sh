13d655a63d938b85fa6491b6f668359c33fe7c74
OKA Toshiyuki <oktopus1959@gmail.com>
2022-07-02 13:39:47 +0900

    Update FAQ

--- tools/faq_toc_diff.sh
+++ tools/faq_toc_diff.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-md=FAQ.md
-diff -w <(tools/make_toc.sh $md) <(sed -n '/## 目次/,/## 設定ダイアログ/ p' $md)
+md=FAQ/FAQ-${1}.md
+diff -w <(tools/make_toc.sh $md) <(sed -n '/## 目次/,/## [^目]/ p' $md)
