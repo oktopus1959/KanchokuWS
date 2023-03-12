cb5e025c884688132ed08a60ccc8c4aa8471afb8
OKA Toshiyuki <oktopus1959@gmail.com>
2023-03-12 18:18:25 +0900

    v1.2.3.1

--- KanchokuWS/Settings.cs
+++ KanchokuWS/Settings.cs
@@ -16,7 +16,7 @@ namespace KanchokuWS
 
         //-------------------------------------------------------------------------------------
         /// <summary> バージョン </summary>
-        public static string Version => "1.2.3";
+        public static string Version => "1.2.3.1";
         public static string Version2 => "";
 
         //-------------------------------------------------------------------------------------
