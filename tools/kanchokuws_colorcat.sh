99fbdb9b0c6b80843c04f722e3119dddb11168d4
OKA Toshiyuki <oka.oktopus@gmail.com>
2023-04-15 17:54:22 +0900

    Call colorcat.sh in same bin dir

--- tools/kanchokuws_colorcat.sh
+++ tools/kanchokuws_colorcat.sh
@@ -1,13 +1,15 @@
 #! /bin/bash
 
-colorcat.sh 'Determiner.*(ENTER|LEAVE)|COMBO.*(PASSED|FOUND|NOT|FAILED)|(result|hotList|comboList|unprocList)=[0-9:]+|TIMER. (STARTED|ELAPSED)|TRY NEXT:.*|(NO )?RULE[ (].*|bTemporaryComboDisabled=True\
+BINDIR=$(dirname $0)
+
+$BINDIR/colorcat.sh 'Determiner.*(ENTER|LEAVE)|COMBO.*(PASSED|FOUND|NOT|FAILED)|(result|hotList|comboList|unprocList)=[0-9:]+|TIMER. (STARTED|ELAPSED)|TRY NEXT:.*|(NO )?RULE[ (].*|bTemporaryComboDisabled=True\
 |No combo shift key|Single Hittable or SequentialShift|sendVkeyFromDeckey|SandS|vkeyQueue.Count=.*|KeyboardEventHandler.invokeHandler|cancelPreRewrite|LatticeState\
 |ChangeCurrentPoolByDecoderMode.*|Imediate.*check|KeyboardHook.HookProcedure.*Key(Up|Down)|Handle as SingleHit: .*ms|TIMER-A (STARTED|ELAPSED)\
 |left=0, top=0, width=0, height=0|MOVE: X=0, Y=0, W=0, H=0|VirtualKeys.ReadExtra.* line\([0-9]+\):|COMBO SEARCH: searchKey=.*|IsTemporaryComboDisabled=\w*\
 | CurrentLine:[0-9]+:.*|Analyze for .* mode|extraInfo=1959' | \
-colorcat.sh -r 'WARN.*' | \
-colorcat.sh -y ' WARN ' | \
-colorcat.sh -r ' ERROR ' | \
-colorcat.sh -c ' INFOH? |ENTER(: deckey=[^,]*, mod)|ENTER|LEAVE|CALLED|==== TEST\([0-9]+\):.* ====' | \
-colorcat.sh -b 'GetKeyCombinationWhenKeyUp|_findCombo|_isCombinationTiming' | \
-colorcat.sh -g '[、。ぁ-龠]+'
+$BINDIR/colorcat.sh -r 'WARN.*' | \
+$BINDIR/colorcat.sh -y ' WARN ' | \
+$BINDIR/colorcat.sh -r ' ERROR ' | \
+$BINDIR/colorcat.sh -c ' INFOH? |ENTER(: deckey=[^,]*, mod)|ENTER|LEAVE|CALLED|==== TEST\([0-9]+\):.* ====' | \
+$BINDIR/colorcat.sh -b 'GetKeyCombinationWhenKeyUp|_findCombo|_isCombinationTiming' | \
+$BINDIR/colorcat.sh -g '[、。ぁ-龠]+'
--- tools/kw-uni_colorcat.sh
+++ tools/kw-uni_colorcat.sh
@@ -1,14 +1,16 @@
 #! /bin/bash
 
+BINDIR=$(dirname $0)
+
 cat $* | \
-    colorcat.sh \
+    $BINDIR/colorcat.sh \
 'createRewriteNode|PostRewriteOneShotNode merged|makeSubTree|createNode.*(ENTER|LEAVE)|OUTPUT_STACK->setRewritable| targetStr=|tail_size_while_only_and_upto|ADD reversePath\
 |RootStrokeNode[12]|StrokeMergerState|[Dd]estructor|LatticeImpl|DecoderImpl.HandleDeckey|MecabBridge.mecabCalcCost|ABANDON|handleStrokeKeys|StrokeMergerState.HandleDeckey\
 |deckey=.* statesNum=.*|handleDeckey_single|BushuAssocExState|PostRewriteOneShotState|DoProcOnCreated|StrokeTableState.HandleDeckey|State.dispatchDeckey|CurrentStrokeTable=[12]\
 |[Cc]reateStrokeTree[123]?|Eisu(State)?|: (ja|LastJapanese)Key=.*|NAMESPACE.RomanToKatakana' | \
-    colorcat.sh -r 'WARN.*' | \
-    colorcat.sh -y ' WARN ' | \
-    colorcat.sh -r ' ERROR ' | \
-    colorcat.sh -c ' INFOH? |ENTER(: deckey=[^,]*, mod)|ENTER|LEAVE|CALLED|==== TEST\([0-9]+\):.* ====' | \
-    colorcat.sh -b 'GetKeyCombinationWhenKeyUp|_findCombo|_isCombinationTiming' | \
-    colorcat.sh -g '[、。ぁ-龠]+'
+    $BINDIR/colorcat.sh -r 'WARN.*' | \
+    $BINDIR/colorcat.sh -y ' WARN ' | \
+    $BINDIR/colorcat.sh -r ' ERROR ' | \
+    $BINDIR/colorcat.sh -c ' INFOH? |ENTER(: deckey=[^,]*, mod)|ENTER|LEAVE|CALLED|==== TEST\([0-9]+\):.* ====' | \
+    $BINDIR/colorcat.sh -b 'GetKeyCombinationWhenKeyUp|_findCombo|_isCombinationTiming' | \
+    $BINDIR/colorcat.sh -g '[、。ぁ-龠]+'
